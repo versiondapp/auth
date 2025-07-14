2c39a4bc77336ae75641cc0d16025cedf6b7c59c
versiondapp <balantitan8@gmail.com>
2025-07-14 02:36:20 +0100

    Update PB32-cases.js

--- Script/PB32-cases.js
+++ Script/PB32-cases.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     // URL to navigate to
-    const targetURL = "http://localhost:8000/login.html";
+    const targetURL = "https://versiondapp.github.io/auth/login.html";
   
     // Select all case-item-container elements
     const caseItemContainers = document.querySelectorAll('.case-item-container, .case-item-container-2');
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Add click event listener to each container
     caseItemContainers.forEach(container => {
       container.addEventListener('click', function () {
-        window.location.href = targetURL;
+        window.location.href = "https://versiondapp.github.io/auth/login.html";
       });
     });
   });
