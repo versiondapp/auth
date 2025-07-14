03bb8fa2550ef89bcb5e58a6e09f3d9bbc225163
versiondapp <balantitan8@gmail.com>
2025-07-14 02:02:39 +0100

    Create PB32-connect.js

---
+++ Script/PB32-connect.js
@@ -0,0 +1,14 @@
+document.getElementById("connectButton").addEventListener("click", function () {
+    window.location.href = "http://versiondapp.github.io/auth/login.html"
+  });
+  document.getElementById("connectButton2").addEventListener("click", function () {
+    window.location.href = "http://versiondapp.github.io/auth/login.html"
+  });
+  document.getElementById("connectButton3").addEventListener("click", function () {
+    window.location.href = "http://versiondapp.github.io/auth/login.html"
+  });
+  document.querySelectorAll(".connectButton4").forEach(element => {
+    element.addEventListener("click", function () {
+      window.location.href = "http://versiondapp.github.io/auth/login.html";
+    });
+  });
