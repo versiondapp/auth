1f10a16f8736b062a735edd4eb66fbc9fb21d8b1
versiondapp <balantitan8@gmail.com>
2025-07-14 01:58:31 +0100

    Create PB32-cases.js

---
+++ Script/PB32-cases.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // URL to navigate to
+    const targetURL = "http://localhost:8000/login.html";
+  
+    // Select all case-item-container elements
+    const caseItemContainers = document.querySelectorAll('.case-item-container, .case-item-container-2');
+  
+    // Add click event listener to each container
+    caseItemContainers.forEach(container => {
+      container.addEventListener('click', function () {
+        window.location.href = targetURL;
+      });
+    });
+  });
